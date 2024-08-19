e5b9f2350cbf4dea990fcf8d4fdc238431899592
Nikos Kontakis <wirednkod@gmail.com>
2024-08-19 22:45:26 +0200

    Add minor types fix for PAPI

--- src/clients.tsx
+++ src/clients.tsx
@@ -1,6 +1,6 @@
 // `dot` is the name we gave to `npx papi add`
 import { dot, ksm } from '@polkadot-api/descriptors'
-import { createClient } from 'polkadot-api'
+import { TypedApi, createClient } from 'polkadot-api'
 import { getWsProvider } from 'polkadot-api/ws-provider/web'
 
 // Connect to the polkadot relay chain.
@@ -13,5 +13,5 @@ export const ksmClient = createClient(
 
 // To interact with the chain, you need to get the `TypedApi`, which includes
 // all the types for every call in that chain:
-export const dotApi = dotClient.getTypedApi(dot)
-export const kusamaApi = ksmClient.getTypedApi(ksm)
+export const dotApi: TypedApi<typeof dot> = dotClient.getTypedApi(dot)
+export const kusamaApi: TypedApi<typeof ksm> = ksmClient.getTypedApi(ksm)
