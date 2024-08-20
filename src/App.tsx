ceca85938712ab13c5f5e0662f81bbb4877b56ee
Thibaut Sardan <github@thib.top>
2024-08-20 22:46:10 +0200

    locks and votes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { AccountContextProvider } from './contexts/AccountsContext'
 import { LocksContextProvider } from './contexts/LocksContext'
 import { DelegateeContextProvider } from '@/contexts/DelegateesContext'
 import { NetworkContextProvider } from './contexts/NetworkContext'
+import { LocksContextProvider } from './contexts/LocksContext'
 
 const App = () => {
   const [settings] = useLocalStorage('fellowship-settings', {
