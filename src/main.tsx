b20693e76afeee7e27b0baca8dce1cf99a4b9387
Nikos Kontakis <wirednkod@gmail.com>
2024-08-25 19:08:01 +0300

    Convert Hashrouter to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
@@ -11,8 +11,8 @@ const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <HashRouter basename="/">
+    <BrowserRouter basename="/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
