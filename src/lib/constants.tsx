47dcb44c24f75bdd2649b55bdfa6b54148e005a8
Thibaut Sardan <github@thib.top>
2024-08-28 17:54:44 +0200

    fix network

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -48,12 +48,6 @@ const msgs: Record<string, MsgType> = {
   },
 }
 
-const supportedNetworksChainIds = {
-  polkadot:
-    '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
-  kusama: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe',
-}
-
 // Exports
 export {
   // Site specifics
@@ -67,7 +61,6 @@ export {
   DelegeeListPolkadot,
   DelegeeListKusama,
   GithubOwner,
-  supportedNetworksChainIds,
   // Alert messsages
   msgs,
 }
