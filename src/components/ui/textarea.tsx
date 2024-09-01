624ab1b6fc554c036ccb4e5505db507f4b0b68b7
Thibaut Sardan <github@thib.top>
2024-09-01 12:47:08 +0200

    silence eslint

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
