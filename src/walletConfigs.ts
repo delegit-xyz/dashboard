49dfdf97b338a52c173a85e7d2fa99872cc19c9d
Nikos Kontakis <wirednkod@gmail.com>
2024-09-03 21:02:49 +0300

    more clear ups of dotconnect

--- src/walletConfigs.ts
+++ src/walletConfigs.ts
@@ -1,7 +1,4 @@
 import { dot } from '@polkadot-api/descriptors'
-import type { Config } from '@reactive-dot/core'
-import { InjectedWalletAggregator } from '@reactive-dot/core/wallets.js'
-import { registerDotConnect } from 'dot-connect'
 import { getWsProvider } from 'polkadot-api/ws-provider/web'
 
 export const config = {
@@ -11,10 +8,4 @@ export const config = {
       provider: getWsProvider('wss://dot-rpc.stakeworld.io'),
     },
   },
-  wallets: [new InjectedWalletAggregator()],
-} satisfies Config
-
-// Register dot-connect custom elements & configure supported wallets
-registerDotConnect({
-  wallets: config.wallets,
-})
+}
