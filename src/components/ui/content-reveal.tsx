4708cef453a5a5a6cc3cdab459d7fe87031bbbfa
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2024-09-06 13:11:24 +0200

    Fix long list (#100)

--- src/components/ui/content-reveal.tsx
+++ src/components/ui/content-reveal.tsx
@@ -1,14 +1,22 @@
 import { cn } from '@/lib/utils'
 import { ChevronDown } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 type Props = {
   children: React.ReactNode
   className?: string
   hidden?: boolean
+  title?: string | React.ReactNode
+  buttonClassName?: string
 }
 
-export const ContentReveal = ({ children, className, hidden }: Props) => {
+export const ContentReveal = ({
+  children,
+  className,
+  hidden = false,
+  title = '',
+  buttonClassName = '',
+}: Props) => {
   const [isOpen, setIsOpen] = useState(false)
 
   useEffect(() => {
@@ -17,18 +25,24 @@ export const ContentReveal = ({ children, className, hidden }: Props) => {
     }
   }, [hidden])
 
+  const toggle = useCallback(() => {
+    if (hidden) return
+
+    setIsOpen((prev) => !prev)
+  }, [hidden])
+
   return (
     <div className={cn('break-anywhere', className)}>
       <button
-        onClick={() => {
-          !hidden && setIsOpen(!isOpen)
-        }}
+        onClick={toggle}
         className={cn(
           `flex w-full items-center justify-center`,
           hidden && 'opacity-0',
+          buttonClassName,
         )}
         disabled={hidden}
       >
+        {title && <span className="mr-2">{title}</span>}
         <ChevronDown
           className={`transition-transform duration-300 ${
             isOpen ? 'rotate-180' : ''
