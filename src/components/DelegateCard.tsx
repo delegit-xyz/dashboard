9065690f375f1cb4e7dca1102d4884f8d9a64dab
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2024-09-16 18:26:53 +0200

    Display markdown in descriptions (#145)

--- src/components/DelegateCard.tsx
+++ src/components/DelegateCard.tsx
@@ -1,48 +1,52 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
 import { Delegate } from '@/contexts/DelegatesContext'
 import { ContentReveal } from './ui/content-reveal'
+import Markdown from 'react-markdown'
+import { Title, TitleH2, TitleH3 } from './ui/title'
+import { AnchorLink } from './ui/anchorLink'
 
 interface Props {
   delegate: Delegate
 }
-export const DelegateCard = ({ delegate: d }: Props) => {
-  const [copied, setCopied] = useState<boolean>(false)
+export const DelegateCard = ({
+  delegate: { address, longDescription, shortDescription, image, name },
+}: Props) => {
   const navigate = useNavigate()
   const { search } = useLocation()
-
-  useEffect(() => {
-    if (copied) {
-      setTimeout(() => setCopied(false), 1000)
-    }
-  }, [copied])
+  const shouldHideLongDescription =
+    !longDescription || longDescription === shortDescription
 
   const onDelegate = () => {
-    navigate(`/delegate/${d.address}${search}`)
+    navigate(`/delegate/${address}${search}`)
   }
 
   return (
     <Card className="mb-5 flex flex-col border p-2">
       <div className="flex flex-col md:flex-row">
         <div className="vertical center p-2">
-          <img className="rounded-full" width="100" src={d.image} />
+          <img className="rounded-full" width="100" src={image} />
         </div>
         <div className="p-2 md:w-[85%]">
-          <div className="py-2 text-xl font-semibold">{d.name}</div>
+          <div className="py-2 text-xl font-semibold">{name}</div>
           <div className="text-slate-600">
-            <div className="break-words">{d.shortDescription}</div>
-            <ContentReveal
-              hidden={
-                d.shortDescription === d.longDescription || !d.longDescription
-              }
-            >
-              {d.longDescription}
+            <div className="break-words">{shortDescription}</div>
+            <ContentReveal hidden={shouldHideLongDescription}>
+              <Markdown
+                components={{
+                  h1: Title,
+                  h2: TitleH2,
+                  h3: TitleH3,
+                  a: AnchorLink,
+                }}
+              >
+                {longDescription}
+              </Markdown>
             </ContentReveal>
           </div>
         </div>
-        <Button variant="default" onClick={onDelegate} className="">
+        <Button variant="default" onClick={onDelegate}>
           Delegate
         </Button>
       </div>
