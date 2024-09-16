9df13037da33c0b793b678de2616bdaaf0427e44
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2024-09-16 14:09:17 +0200

    Randomize delegates (#144)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -78,3 +78,24 @@ export const getLockTimes = async (api: ApiType) => {
     {} as Record<string, bigint>,
   )
 }
+
+// thanks to https://stackoverflow.com/a/2450976/3086912
+export const shuffleArray = (arrayToShuffle: unknown[]) => {
+  const array = [...arrayToShuffle]
+  let currentIndex = array.length
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+
+  return array
+}
