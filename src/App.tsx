6ea5ca4e286f62de313e818a693d21ab7b8689a4
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-09-19 01:10:54 +1200

    refactor: dot-connect usage
    - Bump dependencies
    - Remove chain logic since only wallet capabilities are used
    - Set `color-scheme` CSS property for dark-mode syncing
    - Use DApp color palette

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './index.css'
 import { Content } from './Content'
 import 'dot-connect/font.css'
 import { config } from './walletConfigs'
-import { ReDotProvider, ReDotChainProvider } from '@reactive-dot/react'
+import { ReactiveDotProvider } from '@reactive-dot/react'
 import { Suspense } from 'react'
 import { AccountContextProvider } from './contexts/AccountsContext'
 import { LocksContextProvider } from './contexts/LocksContext'
@@ -25,29 +25,27 @@ const App = () => {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme={settings?.themeMode as Theme}>
-        <ReDotProvider config={config}>
-          <ReDotChainProvider chainId="polkadot">
-            <Suspense fallback={<div>Loading...</div>}>
-              <NetworkContextProvider>
-                <DelegateContextProvider>
-                  <AccountContextProvider>
-                    <LocksContextProvider>
-                      <TooltipProvider>
-                        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-                          <Navigation />
-                          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                            <Header />
-                            <Content />
-                          </div>
+        <ReactiveDotProvider config={config}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <NetworkContextProvider>
+              <DelegateContextProvider>
+                <AccountContextProvider>
+                  <LocksContextProvider>
+                    <TooltipProvider>
+                      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+                        <Navigation />
+                        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                          <Header />
+                          <Content />
                         </div>
-                      </TooltipProvider>
-                    </LocksContextProvider>
-                  </AccountContextProvider>
-                </DelegateContextProvider>
-              </NetworkContextProvider>
-            </Suspense>
-          </ReDotChainProvider>
-        </ReDotProvider>
+                      </div>
+                    </TooltipProvider>
+                  </LocksContextProvider>
+                </AccountContextProvider>
+              </DelegateContextProvider>
+            </NetworkContextProvider>
+          </Suspense>
+        </ReactiveDotProvider>
       </ThemeProvider>
       <Toaster />
     </ErrorBoundary>
--- src/walletConfigs.ts
+++ src/walletConfigs.ts
@@ -1,16 +1,9 @@
-import { dot } from '@polkadot-api/descriptors'
 import type { Config } from '@reactive-dot/core'
 import { InjectedWalletAggregator } from '@reactive-dot/core/wallets.js'
 import { registerDotConnect } from 'dot-connect'
-import { getWsProvider } from 'polkadot-api/ws-provider/web'
 
 export const config = {
-  chains: {
-    polkadot: {
-      descriptor: dot,
-      provider: getWsProvider('wss://dot-rpc.stakeworld.io'),
-    },
-  },
+  chains: {},
   wallets: [new InjectedWalletAggregator()],
 } satisfies Config
 
